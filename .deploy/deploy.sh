6e1d2ba3b73b463d14d1472fb7dbbf3dd714e2d5
Danilo Lutz <danilolutz@gmail.com>
2018-12-23 15:07:59 -0200

    Feature: auto publish nuget package by travis.

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -2,4 +2,4 @@ ApiKey=$1
 
 dotnet pack CoreZipCode.sln -v detailed
 
-dotnet nuget push CoreZipCode.*.nupkg --api-key $ApiKey --source https://api.nuget.org/v3/index.json
+dotnet nuget push CoreZipCode/bin/Debug/CoreZipCode.*.nupkg --api-key $ApiKey --source https://api.nuget.org/v3/index.json
