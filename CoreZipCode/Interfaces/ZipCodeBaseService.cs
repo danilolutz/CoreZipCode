cde09ee4ac332be5ce617d5e5eb8dc9ee036de50
Danilo Lutz <danilolutz@gmail.com>
2018-12-26 16:31:49 -0200

    WIP: Feature: new method calls * Some improvements on ZipCodeBaseService class * GetAddress witch returns a typed object * ListAddresses witch returns a List of type objects * Unit tests for things above [skip travis]

--- CoreZipCode/Interfaces/ZipCodeBaseService.cs
+++ CoreZipCode/Interfaces/ZipCodeBaseService.cs
@@ -1,42 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace CoreZipCode.Interfaces
 {
     public abstract class ZipCodeBaseService
     {
-        private string Url { get; set; }
-        
-        private string Execute()
+        private string CallApi(string url)
         {
             try
             {
                 var request = new HttpClient();
-                var response = request.GetAsync(Url).Result;
+                var response = request.GetAsync(url).Result;
 
                 if (response.StatusCode == HttpStatusCode.BadRequest)
                     throw new ArgumentException();
 
                 return response.Content.ReadAsStringAsync().Result;
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
-                throw new System.Exception($"Error trying execute the request: {ex.Message}");
+                throw new Exception($"Error trying execute the request: {ex.Message}");
             }
         }
-        
-        public string Execute(string zipcode)
-        {
-            Url = SetZipCodeUrl(zipcode);
-            return Execute();
-        }
 
-        public string Execute(string state, string city, string street)
-        {
-            Url = SetZipCodeUrlBy(state, city, street);
-            return Execute();
-        }
+        public string Execute(string zipcode) => CallApi(SetZipCodeUrl(zipcode));
+
+        public string Execute(string state, string city, string street) => CallApi(SetZipCodeUrlBy(state, city, street));
+
+        public T GetAddress<T>(string zipcode) => JsonConvert.DeserializeObject<T>(CallApi(SetZipCodeUrl(zipcode)));
+
+        public IList<T> ListAddresses<T>(string state, string city, string street) => JsonConvert.DeserializeObject<IList<T>>(CallApi(SetZipCodeUrlBy(state, city, street)));
 
         public abstract string SetZipCodeUrl(string zipcode);
         public abstract string SetZipCodeUrlBy(string state, string city, string street);
