081b253fc520df54eb277daf6852d75a791ffd58
Danilo Lutz <danilolutz@gmail.com>
2019-01-10 15:30:07 -0200

    Update ViaCepTest.cs

--- CoreZipCode.Tests/Services/ViaCepTest.cs
+++ CoreZipCode.Tests/Services/ViaCepTest.cs
@@ -16,7 +16,7 @@ namespace CoreZipCode.Tests.Services
         [Fact]
         public void MustGetSingleZipCodeJsonString()
         {
-            string expected = "a{\n  \"cep\": \"14810-100\",\n  \"logradouro\": \"Rua Barão do Rio Branco\",\n  \"complemento\": \"\",\n  \"bairro\": \"Vila Xavier (Vila Xavier)\",\n  \"localidade\": \"Araraquara\",\n  \"uf\": \"SP\",\n  \"unidade\": \"\",\n  \"ibge\": \"3503208\",\n  \"gia\": \"1818\"\n}";
+            string expected = "{\n  \"cep\": \"14810-100\",\n  \"logradouro\": \"Rua Barão do Rio Branco\",\n  \"complemento\": \"\",\n  \"bairro\": \"Vila Xavier (Vila Xavier)\",\n  \"localidade\": \"Araraquara\",\n  \"uf\": \"SP\",\n  \"unidade\": \"\",\n  \"ibge\": \"3503208\",\n  \"gia\": \"1818\"\n}";
             var actual = _service.Execute("14810-100");
 
             Assert.Equal(expected, actual);
