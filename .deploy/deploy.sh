22bf0264db3f67537a4d3f917689eb406ccc6264
Danilo Lutz <danilolutz@gmail.com>
2019-02-08 12:42:19 -0200

    Fixing Travis CI

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-dotnet nuget push CoreZipCode/bin/Debug/CoreZipCode.${TRAVIS_TAG#v}.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push CoreZipCode/bin/Release/CoreZipCode.${TRAVIS_TAG#v}.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
